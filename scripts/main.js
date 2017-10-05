6ea1e5d9f023723f73cf6cc7575b956fc9b8242e
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2017-10-05 19:58:16 +0200

    Better adding tasks.

--- scripts/main.js
+++ scripts/main.js
@@ -6,17 +6,26 @@ removeButton.classList.add('btn','btn-danger');
 removeButton.appendChild(removeButtonContent);
 
 function addNewTask() {
-    const removeButton = document.createElement("button");
-    const removeButtonContent = document.createTextNode('Remove');
-    removeButton.classList.add('btn','btn-danger');
-    removeButton.appendChild(removeButtonContent);
+
 
     let listItem = document.createElement("li");
     listItem.classList.add('list-group-item','d-flex','justify-content-between','align-items-center');
-    let newTask = document.createTextNode(newTaskContent.value);
-    listItem.appendChild(newTask);
-    listItem.appendChild(removeButton);
-    document.querySelector('#taskList').appendChild(listItem);
+    if(newTaskContent.value=='') {
+      alert('Task cannot be empty!');
+    }
+    else {
+      let newTask = document.createTextNode(newTaskContent.value);
+      listItem.appendChild(newTask);
+      document.querySelector('#taskList').appendChild(listItem);
+      const removeButton = document.createElement("button");
+      const removeButtonContent = document.createTextNode('Remove');
+      removeButton.classList.add('btn','btn-danger');
+      removeButton.appendChild(removeButtonContent);
+      listItem.appendChild(removeButton);
+    }
 }
+function removeTask() {
 
+}
 addTask.addEventListener('click', addNewTask);
+removeTask.addEventListener('click', addNewTask);
