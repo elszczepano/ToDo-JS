22e243d65fdc582fdb468e1279bfe34c7b843488
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2017-10-09 20:54:14 +0200

    Now the code is less redundant. Unnecessary functions are deleted.

--- scripts/app.js
+++ scripts/app.js
@@ -1,40 +1,29 @@
 const addTask = document.querySelector('#addTaskButton');
 const list = document.querySelector('#taskList');
-let myTasks = [];
 let lastKey = 0;
-function loadTasksFromStorage() {
-  myTasks = [];
-  for (let key in localStorage) {
-  myTasks[key] = localStorage.getItem(key);
-  lastKey = key;
-  }
-  loadTasks();
-}
-function showTasks(task, index) {
+function showTasks(key) {
   let listItem = document.createElement("li");
   listItem.innerHTML = `<div class="input-group">
-  <input type="text" class="form-control"a value="${task}" readonly>
+  <input type="text" class="form-control"a value="${localStorage.getItem(key)}" readonly>
   <span class="input-group-btn">
     <button class="btn btn-danger"  type="button">Remove</button>
   </span>
   </div>`;
   list.appendChild(listItem);
-
   let removeButton = listItem.querySelector('.btn-danger');
   removeButton.addEventListener('click',function(){
     removeTask(this);
-    delete myTasks[index];
-    localStorage.removeItem(index);
+    localStorage.removeItem(key);
   });
 }
-
 function loadTasks() {
   document.querySelector('#taskList').innerHTML="";
-  myTasks.forEach(function(task, index){
-    showTasks(task, index);
-  });
+  for (let key in localStorage) {
+  localStorage.getItem(key);
+  showTasks(key);
+  lastKey = key;
+  }
 }
-
 function addNewTask() {
     let newTaskContent = document.querySelector('#task');
     if(newTaskContent.value=='') {
@@ -42,16 +31,13 @@ function addNewTask() {
     }
     else {
       localStorage.setItem(++lastKey, newTaskContent.value);
-      myTasks.push(newTaskContent.value);
       newTaskContent.value = "";
       loadTasks();
     }
 }
-
 function removeTask(task) {
   let toRemove = task.closest('li');
   task.closest('ul').removeChild(toRemove);
 }
-
-document.addEventListener('DOMContentLoaded',loadTasksFromStorage);
+document.addEventListener('DOMContentLoaded',loadTasks);
 addTask.addEventListener('click', addNewTask);
